74fdd105346a0ee7b5cb65bfd0c4cfabf8685202
Anna Heggie <aheggiejr@gmail.com>
2024-04-29 03:32:07 -0700

    Added Homework5 part1 files and classes

---
+++ src/Homework5/ArrayConverter.java
@@ -0,0 +1,35 @@
+package Homework5;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ArrayConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the length of the array:");
+        int length = scanner.nextInt();
+
+        int[] arr = new int[length];
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++) {
+            arr[i] = random.nextInt(100); // Generate random numbers between 0 and 99
+        }
+
+        List<Integer> list = toList(arr);
+        System.out.println("Array: " + Arrays.toString(arr));
+        System.out.println("List: " + list);
+    }
+
+
+    public static List<Integer> toList(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        for (int num : arr) {
+            list.add(num);
+        }
+        return list;
+    }
+}
---
+++ src/Homework5/WordOccurrenceCalculator.java
@@ -0,0 +1,51 @@
+package Homework5;
+
+import java.util.*;
+
+public class WordOccurrenceCalculator {
+    public static void main(String[] args) {
+        List<String> wordsList = Arrays.asList("bird", "fox", "cat", "bird", "dog", "cat", "bird", "elephant", "cat", "bird");
+
+
+        calcOccurrence(wordsList);
+
+
+        List<WordOccurrence> occurrences = findOccurrence(wordsList);
+        System.out.println(occurrences);
+    }
+
+    public static void calcOccurrence(List<String> wordsList) {
+        Map<String, Integer> wordCount = new HashMap<>();
+        for (String word : wordsList) {
+            wordCount.put(word.toLowerCase(), wordCount.getOrDefault(word.toLowerCase(), 0) + 1);
+        }
+        wordCount.forEach((word, count) -> System.out.println(word + ": " + count));
+    }
+
+    public static List<WordOccurrence> findOccurrence(List<String> wordsList) {
+        Map<String, Integer> wordCount = new HashMap<>();
+        for (String word : wordsList) {
+            wordCount.put(word.toLowerCase(), wordCount.getOrDefault(word.toLowerCase(), 0) + 1);
+        }
+
+        List<WordOccurrence> occurrences = new ArrayList<>();
+        wordCount.forEach((word, count) -> occurrences.add(new WordOccurrence(word, count)));
+
+        return occurrences;
+    }
+}
+
+class WordOccurrence {
+    String name;
+    int occurrence;
+
+    public WordOccurrence(String name, int occurrence) {
+        this.name = name;
+        this.occurrence = occurrence;
+    }
+
+    @Override
+    public String toString() {
+        return "{name: \"" + name + "\", occurrence: " + occurrence + "}";
+    }
+}
